ca9c8cd030f6416c24444cb008dc0704ed2aa3da
RFCanon <Roy@RFCanon.com>
2019-05-01 15:49:22 -0500

    New updates for Heroku deployment

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h&ifgeny@rxal!n@8^30f$ic+*lykk(zzvn5#mm1d9b+)wt@uk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
